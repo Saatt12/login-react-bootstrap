d3ebd8e889a82191ff24f634eda1364cc805fb79
Sergio A <saat.122090@gmail.com>
2022-08-01 21:06:01 -0400

    first sample

--- src/components/login.js
+++ src/components/login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Col, Container, NavLink, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import descarga from './descarga.png' 
+
 
 
 export default function Login() {
